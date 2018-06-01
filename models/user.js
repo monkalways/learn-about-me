c3799a5f17c1bd6786ff95bd37266b57a380e1e6
Wei Wei (EPS) <monkalways@gmail.com>
2018-06-01 17:10:04 -0600

    Initial checkin

---
+++ models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
+const SALT_FACTOR = 10;
+
+const userSchema = mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  passowrd: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  displayName: String,
+  bio: String
+});
+
+userSchema.methods.name = () => this.displayName || this.username;
+
+const noop = () => {};
+
+userSchema.pre("save", done => {
+  const user = this;
+  if (!user.isModified("password")) {
+    return done();
+  }
+  bcrypt.genSalt(SALT_FACTOR, (err, salt) => {
+    if (err) {
+      return done(err);
+    }
+    bcrypt.hash(user.passowrd, salt, noop, (err, hashedPassword) => {
+      if (err) {
+        return done(err);
+      }
+      user.passowrd = hashedPassword;
+      done();
+    });
+  });
+});
+
+userSchema.methods.checkPassword = (guess, done) => {
+  bcrypt.compare(guess, this.password, (err, isMatch) => {
+    done(err, isMatch);
+  });
+};
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
